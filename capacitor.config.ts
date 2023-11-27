6d5ec9103872fc6c151ee6f602658e8f202b3758
Leandro1441 <leandronstrab@gmail.com>
2023-11-27 14:40:46 -0300

    parte 1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,8 +6,8 @@ const config: CapacitorConfig = {
   webDir: 'dist/xpto',
   server: {
     // androidScheme: 'https'
-    // url: 'https://banco-xpto.netlify.app/',
-    url: "http://192.168.15.187:4200/",
+    url: 'https://banco-xpto.netlify.app/',
+    // url: "http://192.168.15.187:4200/",
     // hostname: '192.168.15.187:4200',
     cleartext: true
   }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    api: 'http://192.168.15.187:8081'
+    api: 'http://backendtcc-production.up.railway.app'
 };
