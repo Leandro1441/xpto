e16b0f02ac2e73c454690592faf20a0921085a2e
Leandro1441 <leandronstrab@gmail.com>
2023-11-28 16:10:10 -0300

    ajuste cap

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,11 +7,6 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: 'https',
     hostname: 'banco-xpto.netlify.app/',
-
-    // url: "http://192.168.15.187:4200/",
-    // hostname: '192.168.15.187:4200',
-
-
     cleartext: true
   }
 };
